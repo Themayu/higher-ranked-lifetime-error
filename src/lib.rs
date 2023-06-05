a29f29775356f3b72a228d1c72072d02a4852427
Jamie Ridding <jamie.ridding@hotmail.co.uk>
2023-06-05 12:41:33 +0100

    higher-ranked lifetime error

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use bevy::reflect::{Reflect, GetTypeRegistration};
+
+#[derive(Reflect)]
+pub struct ErrorStruct {
+    #[reflect(ignore)]
+    hkt: for<'a> fn(&'a str) -> &'a str,
+}
+
+impl Default for ErrorStruct {
+    fn default() -> Self {
+        ErrorStruct {
+            hkt: |input| input
+        }
+    }
+}
+
+fn takes_hkt<T: GetTypeRegistration>() {}
+
+fn lib() {
+    takes_hkt::<ErrorStruct>();
+}
